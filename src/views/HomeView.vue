14cc44dd7e9a0dfb5320cc955912c21d955afac5
Franson33 <antonchikfr@gmail.com>
2023-07-22 06:52:14 +0300

    add simple counter and its logic
    first implemented it using options api for practice, after rewrite to new pattern using composition api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,33 @@
+<script setup>
+import { ref } from 'vue'
+
+const counter = ref(0)
+
+const increment = () => {
+  counter.value++
+}
+const decrement = () => {
+  counter.value--
+}
+</script>
+
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <button @click="decrement" class="btn">-</button>
+    <span class="cntr">{{ counter }}</span>
+    <button @click="increment" class="btn">+</button>
   </div>
 </template>
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+
+.btn,
+.cntr {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
