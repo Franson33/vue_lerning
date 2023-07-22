ad56adfb8cc05a80adccbd0fe0fab476cf04aec7
Franson33 <antonchikfr@gmail.com>
2023-07-22 11:56:24 +0300

    add some more logic to home-view, to practice different vue methods and concepts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,47 @@
 <script setup>
-import { ref } from 'vue'
+import { reactive, computed } from 'vue'
 
-const counter = ref(0)
+const appTitle = 'My cool app written in Vue'
 
-const increment = () => {
-  counter.value++
+const counterData = reactive({
+  counter: 0,
+  title: 'My counter'
+})
+
+const oddOrEven = computed(() => {
+  return (counterData.counter & 1) === 0 ? 'even' : 'odd'
+})
+
+const increment = (amount) => {
+  counterData.counter += amount
 }
-const decrement = () => {
-  counter.value--
+const decrement = (amount) => {
+  counterData.counter -= amount
 }
+
+
 </script>
 
 <template>
   <div class="home">
-    <button @click="decrement" class="btn">-</button>
-    <span class="cntr">{{ counter }}</span>
-    <button @click="increment" class="btn">+</button>
+
+    <h2>{{ counterData.title }}:</h2>
+
+    <h2>{{ appTitle }}</h2>
+
+    <button @click="decrement(2)" class="btn">--</button>
+    <button @click="decrement(1)" class="btn">-</button>
+    <span class="center">{{ counterData.counter }}</span>
+    <button @click="increment(1)" class="btn">+</button>
+    <button @click="increment(2)" class="btn">++</button>
+
+    <p>The counter is {{ oddOrEven }}</p>
+
+    <div class="edit">
+      <h2>Edit title</h2>
+      <input v-model="counterData.title" type=" text" />
+    </div>
+
   </div>
 </template>
 
@@ -26,8 +52,12 @@ const decrement = () => {
 }
 
 .btn,
-.cntr {
+.center {
   font-size: 40px;
   margin: 10px;
+}
+
+.edit {
+  margin-top: 60px;
 }
 </style>
